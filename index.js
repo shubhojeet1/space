1022e73a9f6d0f7954103228a57bd618860f897a
Shubhojeet Chatterjee <103964132+shubhojeet1@users.noreply.github.com>
2024-12-05 23:44:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-icon').addEventListener('click', () => {
+    const nav = document.querySelector('nav ul');
+    nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
+  });
+  
